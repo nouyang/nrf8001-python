3d8f5bb0c0bf54483871bfada49e20d2c1a7b0ce
Nancy Ouyang <nancy.ouyang@gmail.com>
2015-03-12 10:57:40 -0400

    added example client

---
+++ Readme.md
@@ -0,0 +1,7 @@
+For tutorial
+http://www.orangenarwhals.com/2014/07/python-adafruit-nrf8001-bluetooth-low-energy-breakout-in-20-minutes-ubuntu-14-04/
+
+Moved from gist
+https://gist.github.com/nouyang/2a6a733d8facd23115a4
+
+Contact: nouyang@alum.mit.edu
---
+++ clientdebug.py
@@ -0,0 +1,63 @@
+import swisclient_wp as swisclient
+import time, sys, serial, socket
+import constants
+
+class swarm:    
+
+    def __init__(self, numbots, port):
+        #self.ser = serial.Serial("/dev/ttyUSB0", 9600)
+        self.host = constants.HOST_BTLE
+        self.port = port 
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.swis = swisclient.SwisClient()
+        self.NUM_ROBOTS = numbots 
+        
+        self.sock.connect((self.host,self.port))
+
+        print "__INIT__"
+    # Close the serial connection
+    def closeSerial(self):
+        time.sleep(1)
+    #    self.ser.close()
+
+    def write(self, string):
+        pass
+
+    def closeSocket(self):
+        self.sock.close()
+
+
+    # Uses a PD loop to generate velocity for each robot,
+    # given a list of waypoints.
+    def step(self):
+        message = ""
+
+        # Go through each robot and update its velocity
+        # TODO add functionality to move to next waypoint
+        for i in range(0, self.NUM_ROBOTS):
+            #message = "0," + str(leftVelocity) + "," + str(rightVelocity)
+            message = "0,90,90"
+            print message
+            self.sock.send(message)
+
+    def setVelocity(self, robot, left, right):
+        message = '0, '+ str(left) + ', ' + str(right)
+        print message
+        self.sock.send(message)
+    
+def main():
+    s = swarm(constants.NUM_ROBOTS, constants.PORT_BTLE)
+    while 1:
+        try:
+            s.step()
+            time.sleep(0.02)
+        except (socket.error, KeyboardInterrupt):
+            print "Exiting program, stopping robots"
+            for i in range (0, constants.NUM_ROBOTS):
+                s.setVelocity(0,90,90)
+            #s.closeSerial()
+            s.closeSocket()
+            sys.exit()
+
+if __name__ == '__main__':
+    main()
